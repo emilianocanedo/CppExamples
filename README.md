b81c275b47842fb27f0790b796ae11fe1ce9a531
emilianocanedo <emilianocanedo@gmail.com>
2016-11-05 02:26:21 -0300

    add map.cpp

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # CppCert
-Cpp Cert Examples
+"And no, I'm not a walking C++ dictionary. I do not keep every technical detail in my head at all times. If I did that, I would be a much poorer programmer. I do keep the main points straight in my head most of the time, and I do know where to find the details when I need them." Bjarne Stroustrup
---
+++ STL/AssociativeContainers/map.cpp
@@ -0,0 +1,117 @@
+#include <map>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+
+template<class T>
+void print(T start, T end)
+{
+	while(start != end) {
+		cout << start->first << ":" << start->second << " ";
+		start++;
+	}
+}
+
+template<class T, class U>
+void fillMap(map<T, T>& m, U start, U end)
+{
+	for(; start != end; ++start)
+		m.insert(pair<T, T>(*start, *start));
+}
+
+template<class T, class U>
+void fillMultimap(multimap<T, T>& m, U start, U end)
+{
+	for(; start != end; ++start)
+		m.insert(pair<T, T>(*start, *start));
+}
+
+void check(const pair<map<int, int>::iterator, bool>& result)
+{
+	if(result.second == true)
+		cout << "A new key value (" << result.first->first << ") has been inserted" << endl;
+	else
+		cout << "Insertion failed, key " << result.first->first << " already exists" << endl;
+}
+
+int main()
+{
+	int t[] = {45, 67, 89, 54, 90, 78, 32, 23};
+
+	//default constructor
+	map<int, int> m1;
+	fillMap(m1, t, t+8);
+
+	//iterator constructor
+	map<int, int> m2(m1.begin(), m1.end());
+
+	//copy constructor
+	map<int, int> m3(m2);
+
+	//different comparator
+	map<int, int, greater<int> > m4(m3.begin(), m3.end());
+
+	//assignement
+	map<int, int, greater<int> > m5;
+	m5 = m4;
+
+	//multimap
+	multimap<int, int> multi;
+	fillMultimap(multi, t, t+8);
+
+	cout << "print a map with default comparator (m3)" << endl;
+	print(m3.begin(), m3.end());
+	cout << endl << endl;
+
+	cout << "print a map with other comparator (m5)" << endl;
+	print(m5.begin(), m5.end());
+	cout << endl << endl;
+
+	cout << "print a map with default comparator with reverse iterator (m3)" << endl;
+	print(m3.rbegin(), m3.rend());
+	cout << endl << endl;
+
+	/*
+	cout << "print a map with default comparator with const iterator..." << endl;
+	print(m3.cbegin(), m3.cend());
+	cout << endl << endl;
+	*/
+
+	cout << "m3 is empty?" << endl;
+	cout << (m3.empty() ? "true" : "false") << endl << endl; 
+
+	cout << "m3 size and max_size" << endl;
+	cout << m3.size() << " " << m3.max_size() << endl << endl;
+
+	cout << "access to key '89' in m3 by [] (no multimap)" << endl;
+	cout << m3[89] << endl << endl;
+	
+	cout << "CAUTION: if an element with key x cannot be found inside the map, the container will create an element with such a key" << endl;
+	cout << m3[666] << endl;
+	print(m3.rbegin(), m3.rend());
+	cout << endl << endl;
+	
+	cout << "NEVER use this operator to check if a particular key is stored inside the map. Use either the find() or the count() method..." << endl;
+	cout << "find return a pair with the values:" << endl;
+	cout << (m3.find(89))->second << endl;
+	cout << "count return a boolean:" << endl;
+	cout << (m3.count(77) ? "true" : "false") << endl << endl;
+
+	cout << "Insert a duplicate key in m3" << endl;
+	pair<map<int, int>::iterator, bool> p = m3.insert(pair<int, int>(89, 77));
+	check(p);
+	cout << endl;
+
+	cout << "Insert a new key in m3" << endl;
+	p = m3.insert(pair<int, int>(404, 77));
+	check(p);
+	cout << endl;
+
+	cout << "Insert a duplicate key in multimap" << endl;
+	multi.insert(pair<int, int>(89, 77));
+	print(multi.begin(), multi.end());
+	cout << endl << endl;
+
+	return 0;
+}
